a5b0599c27b4487d008f89f5016a36597b7a5a18
Pietruszko <gabriel.karalus98@gmail.com>
2025-06-08 14:54:21 +0200

    feat: create dashboard view with login required built in decorator

--- account/views.py
+++ account/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from .forms import LoginForm
 
@@ -28,5 +29,15 @@ def user_login(request):
         'account/login.html',
         {
             'form': form,
+        }
+    )
+
+@login_required
+def dashboard(request):
+    return render(
+        request,
+        'account/dashboard.html',
+        {
+            'section': dashboard,
         }
     )
