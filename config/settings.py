8ea7bd80aea467ecf95b17ef30ea0e4a3d00c1ba
Pietruszko <gabriel.karalus98@gmail.com>
2025-06-09 13:53:19 +0200

    chore: add default authentication backend and custom email authbackend to config

--- config/settings.py
+++ config/settings.py
@@ -49,6 +49,11 @@ LOGOUT_URL = 'logout'
 
 EMAIL_BACKEND ='django.core.mail.backends.console.EmailBackend'
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
