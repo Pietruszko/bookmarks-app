6a3aee1280f1effb7ebf80b3cb862ace81c9dde0
Pietruszko <gabriel.karalus98@gmail.com>
2025-06-16 12:47:28 +0200

    feat: add create action to few views that will be shown in activity stream like following, liking, bookmarking, creating profile

--- account/views.py
+++ account/views.py
@@ -11,6 +11,7 @@ from .forms import (
     ProfileEditForm,
 )
 from .models import Profile, Contact
+from actions.utils import create_action
 
 def user_login(request):
     if request.method == 'POST':
@@ -64,6 +65,7 @@ def register(request):
             new_user.save()
             # Create the user profile
             Profile.objects.create(user=new_user)
+            create_action(new_user, 'has created an account')
             return render(
                 request,
                 'account/register_done.html',
@@ -147,6 +149,7 @@ def user_follow(request):
                     user_from=request.user,
                     user_to=user
                 )
+                create_action(request.user, 'is following', user)
             else:
                 Contact.objects.filter(
                     user_from=request.user,
