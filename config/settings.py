7723254b83b4a154dff43af535c58609042372ae
Pietruszko <gabriel.karalus98@gmail.com>
2025-06-16 15:12:14 +0200

    chore: mimetype js and css for debug toolbar

--- config/settings.py
+++ config/settings.py
@@ -177,3 +177,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
+
+if DEBUG:
+    import mimetypes
+    mimetypes.add_type('application/javascript', '.js', True)
+    mimetypes.add_type('text/css', '.css', True)
